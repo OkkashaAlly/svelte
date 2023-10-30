c6a6eb70c8e47ec0594e919de17e0091eeb4c3e2
OkkashaAlly <steamnature006@gmail.com>
2023-10-30 07:09:15 +0300

    7 - Binding to Attributes

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
   const name = "Okkasha Ally";
   const profession = "<u>Software Engineer</u>";
+  const paragraphID = "paragraph";
+  const disabled = false;
 </script>
 
 <main>
   <h1>Hello {name}</h1>
   <div>{@html profession}</div>
+  <p id={paragraphID}>Paragraph</p>
+  <button {disabled}>Bind</button>
 </main>
 
 <style>

24a1e0fb2a64b5c6dd9f07e2ea42fa582ae84e4c
OkkashaAlly <steamnature006@gmail.com>
2023-10-30 07:04:49 +0300

    6 - Binding HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
   const name = "Okkasha Ally";
+  const profession = "<u>Software Engineer</u>";
 </script>
 
-<main>Hello {name}</main>
+<main>
+  <h1>Hello {name}</h1>
+  <div>{@html profession}</div>
+</main>
 
 <style>
 </style>

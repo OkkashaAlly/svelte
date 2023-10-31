333cf1947fbb7d33fcd56834de56e2a73420b26c
OkkashaAlly <steamnature006@gmail.com>
2023-10-31 07:08:03 +0300

    13 - Form Handling (Part 1)

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,45 @@
 <script>
-  let count = 0;
-
-  const handleClick = (event, size) => {
-    console.log("🚀 ~ file: App.svelte:5 ~ handleClick ~ event:", event);
-    count += size;
+  const formValues = {
+    name: "",
+    bio: "",
+    country: "",
+    locations: [],
   };
 </script>
 
 <main>
-  <button on:click={() => (count += 1)}>Count {count}</button>
-  <button on:click={event => handleClick(event, 5)}>Count {count}</button>
-  <button on:click={event => handleClick(event, 10)}>Count {count}</button>
+  <div>
+    <pre>
+      {JSON.stringify(formValues, null, 2)}
+    </pre>
+  </div>
+
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" bind:value={formValues.name} />
+    </div>
+    <div>
+      <label for="bio">Bio</label>
+      <textarea id="bio" bind:value={formValues.bio} />
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" bind:value={formValues.country}>
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="tz">Tanzania</option>
+      </select>
+    </div>
+    <div>
+      <label for="locations">Locations</label>
+      <select id="locations" bind:value={formValues.locations} multiple>
+        <option value="">Select a locations</option>
+        <option value="india">India</option>
+        <option value="tz">Tanzania</option>
+      </select>
+    </div>
+  </form>
 </main>
 
 <style>

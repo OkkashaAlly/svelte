90598d34b75c130e65c99a701f7ddaa7f23b1250
OkkashaAlly <steamnature006@gmail.com>
2023-10-31 06:51:35 +0300

    12 - Event Handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,17 @@
 <script>
-  const name = "Okkasha Ally";
-  const profession = "<u>Software Engineer</u>";
-  const paragraphID = "paragraph";
-  const disabled = false;
-  const status = "success";
-  const promoted = true;
+  let count = 0;
 
-  let number = 0;
-
-  const names = [
-    { first: "Okkasha", last: "Ally" },
-    { first: "Ismail", last: "Mbarouk" },
-  ];
-  
+  const handleClick = (event, size) => {
+    console.log("🚀 ~ file: App.svelte:5 ~ handleClick ~ event:", event);
+    count += size;
+  };
 </script>
 
 <main>
-  {#each names as name, index (name.first)}
-    <h2>{index +1 }: {name.first}  {name.last}</h2>
-  {/each}
-  
-  
-  <h2 class={status}>status</h2>
-  <!-- <p class:promoted={isPromoted}>movie name</p> -->
-  <p class:promoted>movie name</p>
-  <h1>Hello {name}</h1>
-  <div>{@html profession}</div>
-  <p id={paragraphID}>Paragraph</p>
-  <button {disabled}>Bind</button>
-
-  {#if number === 0}
-    <h1>The number is zero</h1>
-  {:else if number > 0}
-    <h1>The number is positive</h1>
-  {:else if number < 0}
-    <h1>The number is negative</h1>
-  {:else}
-    <h1>Is not a number</h1>
-  {/if}
+  <button on:click={() => (count += 1)}>Count {count}</button>
+  <button on:click={event => handleClick(event, 5)}>Count {count}</button>
+  <button on:click={event => handleClick(event, 10)}>Count {count}</button>
 </main>
 
 <style>
-  .danger {
-    color: red;
-  }
-
-  .success {
-    color: green;
-  }
-
-  .promoted {
-    font-style: italic;
-  }
 </style>

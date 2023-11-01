feb4071e8bab361a51e27d479170af45d507d519
OkkashaAlly <steamnature006@gmail.com>
2023-11-01 10:27:22 +0300

    16 - Reactive Statements

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,29 @@
   let lastName = "Ally";
 
   $: fullName = `${firstName} ${lastName}`;
+
+  // $: console.log(`FullName is ${firstName} ${lastName}`)
+  // $: {
+  //   const greet = `FullName is ${firstName} ${lastName}`;
+  //   console.log(greet);
+  // }
+
+  let volume = 0;
+
+  $: if (volume < 0) {
+    volume = 0;
+    alert("Cant go lower 0");
+  } else if (volume > 20) {
+    volume = 20;
+    alert("cant go higher than 20");
+  }
 </script>
 
 <main>
+  <h2>Current volume {volume}</h2>
+  <button on:click={() => (volume += 2)}>Increase</button>
+  <button on:click={() => (volume -= 2)}>Decrease</button>
+
   <button
     on:click={() => {
       firstName = "Okshanaby";

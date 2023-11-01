c77faa81f5a61fe1ea950c619d35130c157893e2
OkkashaAlly <steamnature006@gmail.com>
2023-11-01 10:31:26 +0300

    17 - Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,9 @@
 <script>
-  let firstName = "OKkasha";
-  let lastName = "Ally";
-
-  $: fullName = `${firstName} ${lastName}`;
-
-  // $: console.log(`FullName is ${firstName} ${lastName}`)
-  // $: {
-  //   const greet = `FullName is ${firstName} ${lastName}`;
-  //   console.log(greet);
-  // }
-
-  let volume = 0;
-
-  $: if (volume < 0) {
-    volume = 0;
-    alert("Cant go lower 0");
-  } else if (volume > 20) {
-    volume = 20;
-    alert("cant go higher than 20");
-  }
+  import Greet from "./components/Greet.svelte";
 </script>
 
 <main>
-  <h2>Current volume {volume}</h2>
-  <button on:click={() => (volume += 2)}>Increase</button>
-  <button on:click={() => (volume -= 2)}>Decrease</button>
-
-  <button
-    on:click={() => {
-      firstName = "Okshanaby";
-    }}>Change name</button
-  >
-  <h2>
-    {firstName}{lastName}
-  </h2>
-  <h2>
-    {fullName}
-  </h2>
+  <Greet />
 </main>
 
 <style>
---
+++ src/components/Greet.svelte
@@ -0,0 +1,1 @@
+<h2>Hello Okkasha</h2>

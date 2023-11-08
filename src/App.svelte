6fdb400120562c426f0ea8e4c3f7a2a0c117a19f
OkkashaAlly <steamnature006@gmail.com>
2023-11-08 14:20:49 +0300

    30 - Module Context

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,14 @@
 <script>
-  import TabA from "./components/TabA.svelte";
-  import TabB from "./components/TabB.svelte";
-  import TabC from "./components/TabC.svelte";
+  import Counter, { getTotalCount } from "./components/Counter.svelte";
 
-  let activeTab = TabA;
+  const alertTotalCount = () => alert(getTotalCount());
 </script>
 
 <main>
-  <div class="">
-    <button on:click={() => (activeTab = TabA)}>TabA</button>
-    <button on:click={() => (activeTab = TabB)}>TabB</button>
-    <button on:click={() => (activeTab = TabC)}>TabC</button>
-  </div>
-
-  <svelte:component this={activeTab} />
-
-  <!-- {#if activeTab === "TabA"}
-    <TabA />
-  {/if}
-  {#if activeTab === "TabB"}
-    <TabB />
-  {/if}
-  {#if activeTab === "TabC"}
-    <TabC />
-  {/if} -->
+  <button on:click={alertTotalCount}>alert total count</button>
+  <Counter />
+  <Counter />
+  <Counter />
 </main>
 
 <style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  let totalCount = 0;
+  export const getTotalCount = () => totalCount;
+</script>
+
+<script>
+  let count = 0;
+
+  const increment = () => {
+    count += 1;
+    totalCount += 1;
+  };
+</script>
+
+<h3>Count - {count}</h3>
+<button on:click={increment}>increment</button>
